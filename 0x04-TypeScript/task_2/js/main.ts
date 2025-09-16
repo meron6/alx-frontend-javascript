733e90f157bf0ae735bf309fefe9a153d17bfd73
meron6 <khozameron5@gmail.com>
2025-09-16 18:39:40 +0000

    update

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,57 +1,50 @@
-// Define the Director interface with the required methods
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-// Define the Teacher interface with its required methods
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 
-// Implement the Director class based on the DirectorInterface
 class Director implements DirectorInterface {
   workFromHome(): string {
-    return 'Working from home';
+    return "Working from home";
   }
 
   getCoffeeBreak(): string {
-    return 'Getting a coffee break';
+    return "Getting a coffee break";
   }
 
   workDirectorTasks(): string {
-    return 'Getting to director tasks';
+    return "Getting to director tasks";
   }
 }
 
-// Implement the Teacher class based on the TeacherInterface
 class Teacher implements TeacherInterface {
   workFromHome(): string {
-    return 'Cannot work from home';
+    return "Cannot work from home";
   }
 
   getCoffeeBreak(): string {
-    return 'Cannot have a break';
+    return "Cannot have a break";
   }
 
   workTeacherTasks(): string {
-    return 'Getting to work';
+    return "Getting to work";
   }
 }
 
-// Create a function that returns either a Director or a Teacher
-// instance based on the salary.
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
+  if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
 
-// Log the expected results to the console to verify the function's logic
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+console.log(createEmployee("$500"));
